f415e418059de688061dea9f0500b2e7b2ae77bf
Martin449 <76888581+martino449@users.noreply.github.com>
2024-07-26 22:14:12 +0200

    Update config.py

--- config.py
+++ config.py
@@ -10,28 +10,4 @@ destinations = {
         ".png",
         ".gif"
     ],
-    "Videos": [
-        ".mp4",
-        ".mov",
-        ".avi"
-    ],
-    "Music": [
-        ".mp3",
-        ".wav",
-        ".flac"
-    ],
-    "Archives": [
-        ".zip",
-        ".rar",
-        ".tar.gz"
-    ],
-    "Programs": [
-        ".c",
-        ".cpp",
-        ".java",
-        ".sh",
-        ".bat",
-        ".cs",
-        ".exe"
-    ]
 }
